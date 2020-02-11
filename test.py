8c24e89e6b44f2006cc34d2160542caea8e30daa
Cory_Munselle <cory.munselle@g.austincc.edu>
2020-02-11 11:39:04 -0600

    Removed file creation for directory scanning and split searchDirectory into two separate functions

--- test.py
+++ test.py
@@ -4,11 +4,11 @@
 # Imports for program
 
 import os
+from PIL import Image
 
 # Global variables
 LOCAL_PATH = os.getcwd()
 
-# Beginning of function
 # Saves a path to a text file
 def savePathToFile(directory):
     if os.path.exists(directory):
@@ -16,8 +16,6 @@ def savePathToFile(directory):
             file.write(directory)
     else:
         print("Not a valid directory, please try again")
-    
-    return directory
 
 # Saves the directory listing of the path provided   
 def saveDirToFile(workingDirectory):
@@ -33,43 +31,54 @@ def saveDirToFile(workingDirectory):
     else:
         print("Not a valid directory, please try again")
 
-# Beginning of searchDirectory
 # Searches for files based on a string and opens it if requested
 def searchDirectory(directory):
 #   Local variables
 
     numMatches = 0
-    matchesRemaining = 0
     searchTerm = input("Please enter either the name of the file or a subset of the name: ")
     confirm = ""
     
 #   Function
-    with open('dirList.txt', 'r') as file:
-        while file.readline() != '':
-            line = file.readline()
-            if searchTerm in line:
-                numMatches += 1
-    print(f'There are {numMatches} files that match the search term provided. Listing them now...')
-    with open('dirList.txt', 'r') as file:
-        while file.readline() != '' and confirm != "stop":
-            line = file.readline()
-            line = line.rstrip('\n')
-            matchesRemaining = numMatches
-            if searchTerm in line:
+    with os.scandir(directory) as curDirectory:
+        for entry in curDirectory:
+            if searchTerm in entry.name and entry.is_file():
+                print(entry.name)
+                
+# Opens a file in a provided directory
+def openFiles(directory):
+    
+    searchTerm = input("Please enter either the name of the file or a subset of the name: ")
+    matchesRemaining = 0
+    confirm = ""
+    
+    # Do an initial scan to check how many files match the searchTerm
+    with os.scandir(directory) as curDirectory:
+        for entry in curDirectory:
+            if searchTerm in entry.name and entry.is_file():
+                matchesRemaining += 1
+    # Iterate through the directory and prompt opening for any files in the directory
+    with os.scandir(directory) as curDirectory:
+        for entry in curDirectory:
+            if searchTerm in entry.name and entry.is_file() and matchesRemaining != 0:
                 matchesRemaining -= 1
-                confirm = input(f'File {line} found!\nWould you like to open it? (y/n/stop)')
+                confirm = input(f'File {entry.name} found!\nWould you like to open it? (y/n/stop)')
                 if confirm == 'y':
                     os.chdir(directory)
-                    os.startfile(line)
+                    os.startfile(entry.name)
                     print(f'File opened! There are {matchesRemaining} files remaining.')
                 elif confirm == 'n':
                     print(f'There are {matchesRemaining} files remaining. Moving to the next file...')
                 elif confirm == 'stop':
                     print("Exiting...")
-                    os.chdir(LOCAL_PATH)
+                    curDirectory.close()
+            elif matchesRemaining == 0 and confirm == "":
+                print("No matches found. Returning to main menu...")
+                   
+# def batchRename(directory):
+    
 
-# Beginning of mainMenu
-
+# Presents selection options for user
 def mainMenu():
 	print("MAIN MENU\n",
 	"================================\n",
@@ -77,9 +86,10 @@ def mainMenu():
 	"2: Save a directory to a text file\n",
 	"3: Save directory listing to a file\n",
     "4: Search directory listing for a file\n",
+    "5: Open a specified file\n",
 	"Anything else to quit")
 	choice = input("Choose an option: ")
-	if choice.isdigit() and int(choice) >= 1 and int(choice) <= 4:
+	if choice.isdigit() and int(choice) >= 1 and int(choice) <= 5:
 		return int(choice)
 	else:
 		return 0
@@ -87,18 +97,20 @@ def mainMenu():
 # Beginning of main
         
 def main():
-    directory = input("Please enter the path of the directory you want scanned: ")
+    directory = input("Please enter the path of the directory you want to work in: ")
     choice = mainMenu()
     if choice == 0:
         print("later gamers")
     elif choice == 1:
         print(LOCAL_PATH)
     elif choice == 2:
-        workingDirectory = savePathToFile(directory)
+        savePathToFile(directory)
     elif choice == 3:
-        workingDirectory = saveDirToFile(directory)
+        saveDirToFile(directory)
     elif choice == 4:
         searchDirectory(directory)
+    elif choice == 5:
+        openFiles(directory)
     
 main()
 # End of main
